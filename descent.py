131a0a89e5c8eda595a3597eae384cc6eb062c66
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-05-06 15:04:49 +0300

    working with stop critirea

--- descent.py
+++ descent.py
@@ -1,9 +1,15 @@
 import numpy as np
-def descent(gradient, learn_rate, num_epochs, start_point): #gradient- function that calculates gradient, num_epochs- number of optimization epochs
+stop_criterea = 10**-5
+
+def descent(gradient, learn_rate, start_point): #gradient- function that calculates gradient, num_epochs- number of optimization epochs
     #learn rate is a function recieving x and d and returning learning rate
     x = np.array(start_point)
-    for i in range(num_epochs):
+    i = 0
+    while True:
+        i += 1 #i- number of epochs to convergence
         #print(f'{x}->{gradient(x)}')
         g = -gradient(x)
-        x -= g * learn_rate(x,g)  #might need to send conjugate gradient and not gradient itself
-    return x #retuns optimal x
+        if np.linalg.norm(g) <= stop_criterea:
+            return [i] + list(x)
+        x += g * learn_rate(x,g)  #might need to send conjugate gradient and not gradient itself
+    return [i] + list(x) #retuns optimal x
--- test.py
+++ test.py
@@ -2,21 +2,19 @@ import numpy as np
 import descent as dc 
 import quadratic as qd
 
-stop_critirea = 10**-5
 Q1 = np.array([[10,0],[0,1]])
 Q2 = np.array([[3,0],[0,3]])
 
 grad = qd.gradient(Q1)
 exact_line_search = qd.exact_line_search(Q1)
 
-num_epochs = int(input())
-print(dc.descent(grad, exact_line_search, num_epochs, np.array([-.2,-2])))
+print(dc.descent(grad, exact_line_search, np.array([-.2,-2])))
 
 
-res = (dc.descent(grad, exact_line_search, num_epochs, np.array([-2.0,0.0])))
+res = (dc.descent(grad, exact_line_search, np.array([-2.0,0.0])))
 grad = qd.gradient(Q2)
 exact_line_search = qd.exact_line_search(Q2)
-print(dc.descent(grad, exact_line_search, num_epochs, np.array([-.2,-2])))
+print(dc.descent(grad, exact_line_search, np.array([-.2,-2])))
 
 print(res)
 
