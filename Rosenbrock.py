fix RB Hessian and shape value bugs add the rest of the test cases add mask for line search method Move exact search to Descent file, should consider different file

Rosenbrock.py
@@ -6,7 +6,7 @@ def func():
 
     def f(x):
         y = 0
-        for i in range(x.shape[1]-1):
+        for i in range(x.shape[0]-1):
             y += (1-x[i])**2+100*((x[i+1]-(x[i]**2))**2)
         return y
 
@@ -16,12 +16,12 @@ def func():
 def gradient():
 
     def gf(x):
-        n = x.shape[1]
-        g = np.zeros((n, 1))
+        n = x.shape[0]
+        g = np.zeros(n)
         g[0] = -2 * (1 - x[0]) - 400 * x[0] * (x[1] - (x[0] ** 2))
-        for i in (1,n-1):
+        for i in range(1,n-1):
             g[i] = -2*(1-x[i]) - 400*x[i]*(x[i+1]-(x[i]**2)) + 200*(x[i]-(x[i-1]**2))
-        g[n-1] = 200*(x[n-1]-(x[n-2]**2));
+        g[n-1] = 200*(x[n-1]-(x[n-2]**2))
         return g
 
     return gf
@@ -30,13 +30,13 @@ def gradient():
 def hessian():
 
     def hf(x):
-        n = x.shape[1]
+        n = x.shape[0]
         h = np.zeros((n, n))
         h[0, 0] = 2 + 1200 * x[0]**2 * -400*x[1]
-        h[0, 1] = -400*x[1]
+        h[0, 1] = -400*x[0]
         h[n-1, n-1] = 200
         h[n-1, n-2] = -400*x[n-2]
-        for i in (1,n-1):
+        for i in range(1, n-1):
             h[i, i] = 202 + 1200 * (x[i] ** 2) - 400 * x[i + 1]
             h[i, i - 1] = -400*x[i-1]
             h[i, i + 1] = -400*x[i]

descent.py
@@ -2,6 +2,17 @@ import numpy as np
 stop_criteria = 10**-5
 
 
+def exact_line_search(Q):
+    def els(x, d):  # d- conjugate gradient direction x-current parameters
+        Qd = Q @ d
+        # returns learning rate which yields optimal value
+        # no need regularization due to guarantee ||d|| > 10**-5
+        return -(x.T @ Qd) / (d.T @ Qd)
+        #return -(x.T @ Qd) / f(d)
+
+    return els
+
+
 def inexact_line_search(x, d, func):
     s = 0.25
     b = 0.5
@@ -9,16 +20,18 @@ def inexact_line_search(x, d, func):
     f0 = func(x)
     return armijo(x, d, s, b, a0, func, f0)
 
+
 def armijo(x, d, s, b, a, f, f0):
     bound = d.T@(-d)*a*b
     fi = (f(x + a*b*d) - f0)
-    #print(f'{bound}, {fi}, {s*bound} : {x + a*b*d}')
+    # print(f'{bound}, {fi}, {s*bound} : {x + a*b*d}')
     if (s*bound >= fi and bound <= fi):
         return a*b
     return armijo(x, d, s, b*0.5, a, f, f0)
 
+
 # gradient- function that calculates gradient, num_epochs- number of optimization epochs
-def descent(gradient, learn_rate, start_point, func):
+def descent(func, gradient, start_point, learn_rate, exact_ls):
     # learn rate is a function receiving x and d and returning learning rate
     x = np.array(start_point)
     i = 0
@@ -27,8 +40,11 @@ def descent(gradient, learn_rate, start_point, func):
     while np.linalg.norm(g) > stop_criteria:
         i += 1  # i- number of epochs to convergence
         # print(f'{x}->{gradient(x)}')
+        if exact_ls is True:
+            a = learn_rate(x, g)
+        else:
+            a = learn_rate(x, g, func)
+        x += g * a
         g = -gradient(x)
-        n = learn_rate(np.array(x), g, func)
-        x += g * n
 
     return [i] + list(x)  # returns optimal x

newton_method.py
@@ -5,7 +5,7 @@ from mcholmz import *
 
 
 # def newton_method(f,x,search_type,step_size):
-def newton_method(gradient, hessian, learn_rate, start_point):
+def newton_method(func, gradient, hessian, start_point, learn_rate, exact_ls=True):
     x = np.array(start_point)
     stop_criteria = 10 ** -5
     i = 0
@@ -28,7 +28,11 @@ def newton_method(gradient, hessian, learn_rate, start_point):
         # dk2 = np.linalg.inv(hessian(x)) @ g
         # Q1 = np.array([[10, 0], [0, 1]])
         # a = line_search(qd.func(Q1), gradient, x, dk)
-        x += dk * learn_rate(x, dk)
+        if exact_ls is True:
+            a = learn_rate(x, dk)
+        else:
+            a = learn_rate(x, dk, func)
+        x += dk * a
         g = -gradient(x)
 
     return [i] + list(x)  # returns optimal x

test.py
@@ -2,36 +2,61 @@ import numpy as np
 import descent as dc
 import newton_method as nm
 import quadratic as qd
+import Rosenbrock as rb
 from scipy.optimize import newton
 
 Q1 = np.array([[10, 0], [0, 1]])
 Q2 = np.array([[3, 0], [0, 3]])
 
+fQ1 = qd.func(Q1)
 gradQ1d = qd.gradient(Q1)
 hessianQ1d = qd.hessian(Q1)
-exact_line_searchQ1 = qd.exact_line_search(Q1)
+exact_line_searchQ1 = dc.exact_line_search(Q1)
 
-#print(dc.descent(gradQ1d, dc.inexact_line_search, np.array([-.2, -2]), qd.func(Q1)))
-
-
-# x0 = newton(qd.func(Q2),  np.array([-.2, -2]), fprime=None, args=(), tol=10 ** -5, maxiter=55000, fprime2=None)
-# print("x0:", x0)
-print("case_1_GD:", dc.descent(gradQ1d, exact_line_searchQ1, np.array([-.2, -2])))
-print("case_1_NM:", nm.newton_method(gradQ1d, hessianQ1d, exact_line_searchQ1, np.array([-.2, -2])))
-
-
-print("case_3_GD:", dc.descent(gradQ1d, exact_line_searchQ1, np.array([-2.0, 0.0])))
-print("case_3_NM:", nm.newton_method(gradQ1d, hessianQ1d, exact_line_searchQ1, np.array([-2.0, 0.0])))
-
+fQ2 = qd.func(Q2)
 gradQ2d = qd.gradient(Q2)
 hessianQ2d = qd.hessian(Q2)
-exact_line_searchQ2 = qd.exact_line_search(Q2)
-print("case_2_GD:", dc.descent(gradQ2d, exact_line_searchQ2, np.array([-.2, -2])))
-print("case_2_NM:", nm.newton_method(gradQ2d, hessianQ2d, exact_line_searchQ2, np.array([-.2, -2])))
+exact_line_searchQ2 = dc.exact_line_search(Q2)
+
+x1_2 = np.array([-0.2, -2])
+x3 = np.array([-2, 0.0])
+
+print("case_1_GD:", dc.descent(fQ1, gradQ1d, x1_2, exact_line_searchQ1, exact_ls=True))
+print("case_1_NM:", nm.newton_method(fQ1, gradQ1d, hessianQ1d, x1_2, exact_line_searchQ1, exact_ls=True))
+
+print("case_2_GD:", dc.descent(fQ2, gradQ2d, x1_2, exact_line_searchQ2, exact_ls=True))
+print("case_2_NM:", nm.newton_method(fQ2, gradQ2d, hessianQ2d, x1_2, exact_line_searchQ2, exact_ls=True))
+
+print("case_3_GD:", dc.descent(fQ1, gradQ1d, x3, exact_line_searchQ1, exact_ls=True))
+print("case_3_NM:", nm.newton_method(fQ1, gradQ1d, hessianQ1d, x3, exact_line_searchQ1, exact_ls=True))
+
+print("case_4_GD:", dc.descent(fQ1, gradQ1d, x1_2, dc.inexact_line_search, exact_ls=False))
+print("case_4_NM:", nm.newton_method(fQ1, gradQ1d, hessianQ1d, x1_2, dc.inexact_line_search, exact_ls=False))
+
+print("case_5_GD:", dc.descent(fQ2, gradQ2d, x1_2, dc.inexact_line_search, exact_ls=False))
+print("case_5_NM:", nm.newton_method(fQ2, gradQ2d, hessianQ2d, x1_2, dc.inexact_line_search, exact_ls=False))
+
+print("case_6_GD:", dc.descent(fQ1, gradQ1d, x3, dc.inexact_line_search, exact_ls=False))
+print("case_6_NM:", nm.newton_method(fQ1, gradQ1d, hessianQ1d, x3, dc.inexact_line_search, exact_ls=False))
 
 
-
+# print(dc.descent(gradQ1d, dc.inexact_line_search, np.array([-.2, -2]), qd.func(Q1)))
 
 
 # 2.7 Find the Minimum of the Rosenbrock Function
 # f*=0, plot f(x_k)-f* for GD and NM
+x0 = np.zeros(10)
+print("Rosenbrock_GD:", dc.descent(rb.func(), rb.gradient(), x0, dc.inexact_line_search, False))
+print("Rosenbrock_NM:", nm.newton_method(rb.func(), rb.gradient(), rb.hessian(), x0, dc.inexact_line_search, False))
+
+
+
+
+
+
+
+
+
+
+
+