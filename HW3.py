9a0fdabd2dbd1e2f568a9ae0c4c8c3c1154bc065
dannavon <navon89@gmail.com>
2021-06-04 00:36:33 +0300

    HW3 loss incomplete

--- HW3.py
+++ HW3.py
@@ -5,3 +5,14 @@ def test_func(x):  # 1.3.5
     return x[1]*math.exp(-(x[1]**2 + x[2]**2))
 
 
+def activation(x):  # 1.3.6
+    out = math.tanh(x)
+    d = 1-math.tanh(x)**2
+    return out, d
+
+
+def loss(pred, y):  # 1.3.7
+    l = (pred-y)**2
+    dl_df = l
+    return dl_df
+
