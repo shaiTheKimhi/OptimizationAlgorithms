bcd2e876c2854f787ce7878691edf95d740d2c78
dannavon <navon89@gmail.com>
2021-06-12 12:58:31 +0300

    need to change eval_grad_of_loss_all to be compatible with different w

--- BFGS.py
+++ BFGS.py
@@ -1,84 +1,87 @@
 from mcholmz import modifiedChol
 import numpy as np
 
+
 def inexact_line_search(x, dir, func, grad):
-   sigma = 0.25
-   beta = 0.5
-   a0 = 1
-   f0 = func(x)
-   # s, phi1 = ls.scalar_search_armijo(func, x, d, f0, alpha0=1)
-   return wolf(x, dir, sigma, 1, a0, func, grad, f0, beta)
+    sigma = 0.25
+    beta = 0.5
+    a0 = 1
+    f0 = func(x)
+    # s, phi1 = ls.scalar_search_armijo(func, x, d, f0, alpha0=1)
+    return wolf(x, dir, sigma, 1, a0, func, grad, f0, beta)
 
 
 def wolf(x, d, sigma, b, a0, f, g, f0, cb):
-   c = (g(x) @ d)  # =phi(0)
-   alpha = a0 * b
-   bound = sigma * alpha * (g(x) @ d) + f0
-   # bound = (g(x) @ d)*a*b
-   phi = f(x + alpha * d)
-   g2 = d @ g(x + alpha * d)
-   g1 = 0.9 * c
+    c = (g(x) @ d)  # =phi(0)
+    alpha = a0 * b
+    bound = sigma * alpha * (g(x) @ d) + f0
+    # bound = (g(x) @ d)*a*b
+    phi = f(x + alpha * d)
+    g2 = d @ g(x + alpha * d)
+    g1 = 0.9 * c
 
-   if bound >= phi and g(x+ alpha*d)@d >=  0.9* c:  #0.9 is c2 this is wolfe curvature condition
-      # if s * bound >= phi:
-      return alpha
-   return wolf(x, d, sigma, b * cb, a0, f, g, f0, cb)
+    if bound >= phi and g(x + alpha * d) @ d >= 0.9 * c:  # 0.9 is c2 this is wolfe curvature condition
+        # if s * bound >= phi:
+        return alpha
+    return wolf(x, d, sigma, b * cb, a0, f, g, f0, cb)
+
 
 def update(Bk, pk, qk):
-   pk = pk.reshape(pk.shape[0],1)
-   qk = qk.reshape(pk.shape[0], 1)
-   #Broyden family
-   sk = np.matmul(Bk,qk)
-   tk = sk.T @ qk
-   miuk = pk.T @ qk
-   vk = (1/miuk) * pk - (1/tk) * sk
+    pk = pk.reshape(pk.shape[0], 1)
+    qk = qk.reshape(pk.shape[0], 1)
+    # Broyden family
+    sk = np.matmul(Bk, qk)
+    tk = sk.T @ qk
+    miuk = pk.T @ qk
+    vk = (1 / miuk) * pk - (1 / tk) * sk
 
-   norm = np.linalg.norm
-   return Bk + (1/miuk)*pk@pk.T -(1/tk)*sk@sk.T + tk*vk@vk.T #return Bk+1
+    norm = np.linalg.norm
+    return Bk + (1 / miuk) * pk @ pk.T - (1 / tk) * sk @ sk.T + tk * vk @ vk.T  # return Bk+1
 
-    #Secant equation
-    #v = pk -  Bk @ qk
-    #return Bk - (v @ v.T)/(v @ qk) #B(k+1)
+    # Secant equation
+    # v = pk -  Bk @ qk
+    # return Bk - (v @ v.T)/(v @ qk) #B(k+1)
 
-def BFGS(func, gradient, start_point, learn_rate, exact_ls=True):
-   zero = 10**-10
-   x = np.array(start_point)
-   stop_criteria = 10 ** -5
-   i = 0
 
-   B = np.eye(x.shape[0]) #B0 initialization
-   g = -gradient(x)
-   tod = np.array([start_point])
+def BFGS(func, gradient, start_point, learn_rate, exact_ls=True, stop_criteria=10 ** -5):
+    zero = 10 ** -10
+    x = np.array(start_point)
 
-   prev_x = x
-   prev_g = g
+    i = 0
 
-   while np.linalg.norm(-g) > stop_criteria:
-      i += 1
-      # we want to solve Hdk=-g
-      # 1. find L and d such that H+e=Ldiag(d)L' and LDL'dk=-g
-      # 2. solve Ly=-g where y=DL'dk
-      # 3. solve Dz=y where z=L'dk
-      # 4. solve L'dk=z
-      #L, D, e = modifiedChol(np.linalg.inv(B))
-      #D = np.squeeze(D)
-      #y = np.linalg.solve(L, g)  # forward_substitution(L,g)
-      #z = (y / D)
-      #dk = np.linalg.solve(L.T, z)  # backward_substitution(L,g)
-      dk = B@g
-      if exact_ls is True:
-         a = learn_rate(x, dk)
-      else:
-         a = learn_rate(x, dk, func, gradient)
-      prev_x = np.array(x) #keep old x and g for p and q accordingly
-      prev_g = np.array(g)
+    B = np.eye(x.shape[0])  # B0 initialization
+    g = -gradient(x)
+    tod = np.array([start_point])
 
-      x += dk * a #calc new x and g
-      tod = np.append(tod, [x], axis=0)
-      g = -gradient(x)
+    prev_x = x
+    prev_g = g
 
-      #print(f"{i}:{x}")
+    while np.linalg.norm(-g) > stop_criteria:
+        i += 1
+        # we want to solve Hdk=-g
+        # 1. find L and d such that H+e=Ldiag(d)L' and LDL'dk=-g
+        # 2. solve Ly=-g where y=DL'dk
+        # 3. solve Dz=y where z=L'dk
+        # 4. solve L'dk=z
+        # L, D, e = modifiedChol(np.linalg.inv(B))
+        # D = np.squeeze(D)
+        # y = np.linalg.solve(L, g)  # forward_substitution(L,g)
+        # z = (y / D)
+        # dk = np.linalg.solve(L.T, z)  # backward_substitution(L,g)
+        dk = B @ g
+        if exact_ls is True:
+            a = learn_rate(x, dk)
+        else:
+            a = learn_rate(x, dk, func, gradient)
+        prev_x = np.array(x)  # keep old x and g for p and q accordingly
+        prev_g = np.array(g)
 
-      B = update(B, x-prev_x, -(g-prev_g)) #update approx inv hessian
+        x += dk * a  # calc new x and g
+        tod = np.append(tod, [x], axis=0)
+        g = -gradient(x)
 
-   return i, tod  # returns optimal x
+        # print(f"{i}:{x}")
+
+        B = update(B, x - prev_x, -(g - prev_g))  # update approx inv hessian
+
+    return i, tod  # returns optimal x
--- HW3.py
+++ HW3.py
@@ -1,87 +1,149 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import BFGS as bfgs
+
 
 def test_func(x):  # 1.3.5
-    return x[1] * math.exp(-(x[1] ** 2 + x[2] ** 2))
+    return x[0, :] * np.exp(-(x[0, :] ** 2 + x[1, :] ** 2))
 
 
-def activation(x):  # 1.3.6
-    out = math.tanh(x)
-    d = 1 - (math.tanh(x) ** 2)
-    return out, d
-
-
-def activation_grad(x):  # 1.3.6
-    val = np.exp(x)
-    return 4 / ((val + 1 / val) ** 2)
-
-
-def loss(estimated, truth, n):  # estimated is F(x;W)- the output of the model, truth= y the given labels
-    return (np.sum((estimated - truth) ** 2)) / n
+def activation(x, der=False):  # 1.3.6
+    if der is False:
+        return np.tanh(x)
+    else:
+        return 1 - (np.tanh(x) ** 2)
 
 
 def loss_grad(pred, y):  # 1.3.7
-    return 2 * np.norm(pred - y)
+    return 2 * (pred - y)
 
 
-def layer_eval(x, w, b):
-    x_pre_act = w.T @ x + b
-    x_next = np.array()
-    d_act = np.array()
-    for s in x_pre_act:
-        x_n, d = activation(s)
-        np.append(x_next, x_n)
-        np.append(d_act, d)
-
-    return x_next, d_act
-
-
-def forward_pass(x_s, W, b):
-    x1, d1 = layer_eval(x_s, W[1, :], b[1, :])
-    x2, d2 = layer_eval(x1, W[2, :], b[2, :])
-    F = W[3, :].T @ x2 + b[3, :]
-    return x1, d1, x2, d2, F
-
-
-def eval_loss_grad(x, y):
-    W = 0
-    b = 0
-    x1, d1, x2, d2, F = forward_pass(x, W, b)
-    lg = loss_grad(F, y)
-    dldx = W @ np.diag(activation_grad(d2)) @ lg
-    dldw = x1 @ lg.T @ np.diag(activation_grad(d2))
-    dldb = np.diag(activation_grad(d2)) @ lg.T
-    return 1
+def loss(out, truth, n):  # estimated is F(x;W)- the output of the model, truth= y the given labels
+    return (np.sum((out - truth) ** 2)) / n
 
 
 def gen_training(n):  # 1.3.10
-    return np.random.rand(n, 2) * 4 - 2
+    x = np.random.rand(2, n) * 4 - 2
+    y = test_func(x)
+    return x, y
 
 
 def gen_test(n):  # 1.3.11
-    return np.random.rand(n, 2) * 4 - 2
-
-
-# 1.3.12
-def init_params(): #incomplite
-    b = [np.zeros([4]), np.zeros([3]), np.zeros([1])]
-    W = [np.random.rand(2, 4)/np.sqrt(4), np.random.rand(4, 3)/np.sqrt(3), np.random.rand(3, 1)]
-    return 0
+    return np.random.rand(2, n) * 4 - 2
 
 
 # 1.3.13
-def data_vis(f,data,b): #incomplite
+def data_vis(self, f, x, y, b):  # incomplete
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     X = np.arange(-2, 2, 0.2)
     Y = np.arange(-2, 2, 0.2)
     X, Y = np.meshgrid(X, Y)
-
-
+    Z = f(X,Y)
     # Plot the surface.
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                            linewidth=0, antialiased=False)
+    if b is True:
+        ax.scatter(x, y, f(x, y))
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+class NN:
+    numLayers = 2
+    weights = []
+
+    def __init__(self):
+        # 1.3.12
+        self.weights = [np.row_stack([np.random.rand(2, 4) / np.sqrt(4), np.zeros([4])]),
+                        np.row_stack([np.random.rand(4, 3) / np.sqrt(3), np.zeros([3])]),
+                        np.row_stack([np.random.rand(3, 1) / np.sqrt(1), np.zeros([1])])]
+        self.layer_input = []
+        self.layer_output = []
+
+    def w_vec(self):
+        return np.concatenate((np.reshape(self.weights[0], -1),
+                               np.reshape(self.weights[1], -1),
+                               np.reshape(self.weights[2], -1)))
+
+    # 1.3.8
+    def forward_pass(self, x_s, weights):
+        self.layer_input = []
+        self.layer_output = []
+
+        num_samples = x_s.shape[1]
+
+        x_s_h = np.concatenate([x_s, np.ones([1, num_samples])])
+        layer_in = weights[0].T.dot(x_s_h)
+        self.layer_input.append(layer_in)
+        act = activation(layer_in)
+        self.layer_output.append(act)
+
+        x1_h = np.concatenate([self.layer_output[-1], np.ones([1, num_samples])])
+        layer_in = weights[1].T.dot(x1_h)
+        self.layer_input.append(layer_in)
+
+        act = activation(layer_in)
+        # act = act[:, np.newaxis]
+        self.layer_output.append(act)
+
+        x2_h = np.concatenate([self.layer_output[-1], np.ones([1, num_samples])])
+        layer_in = weights[2].T.dot(x2_h)
+        self.layer_input.append(layer_in)
+        self.layer_output.append(layer_in)
+        return layer_in
+
+    # 1.3.8
+    def eval_grad_of_loss_single(self, x, y):
+        # 1
+        x = x[:, np.newaxis]
+        F = self.forward_pass(x)
+        # 2
+        lg = loss_grad(F, y)
+
+        # 3
+        dldx2 = self.weights[2] @ lg
+        dldx2 = dldx2[0:-1]
+
+        dldw2 = self.layer_output[1] @ lg.T
+        dldw2 = np.concatenate([dldw2, lg])
+
+
+        # 4  ## x=prev layer output
+        dldx1 = self.weights[1] @ np.diag(np.squeeze(activation(self.layer_input[1], True))) @ dldx2
+        dldx1 = dldx1[0:-1]
+
+        dldw1 = self.layer_output[0] @ dldx2.T @ np.diag(np.squeeze(activation(self.layer_input[1], True)))
+        dldw1 = np.concatenate([dldw1, dldx2.T])
+
+        # dldx0 = self.weights[0] @ np.diag(activation(self.layer_output[0], True)) @ dldx1
+        dldw0 = x @ dldx1.T @ np.diag(np.squeeze(activation(self.layer_input[0], True)))
+        dldw0 = np.concatenate([dldw0, dldx1.T])
+
+        return np.concatenate((np.reshape(dldw2, -1),
+                               np.reshape(dldw1, -1),
+                               np.reshape(dldw0, -1)))
+
+    # 1.3.9
+    def eval_grad_of_loss_all(self, x, y):
+        num_samples = x.shape[1]
+        grad = 0
+        for i in range(num_samples):
+            single_loss = self.eval_grad_of_loss_single(x[:, i], y[i])
+            grad += single_loss * ((self.forward_pass(np.expand_dims(x[:, i], axis=1)) - y[i]) ** 2) / num_samples
+
+        return grad
+
 # 1.3.14
-train_set = gen_training(500)
+nn = NN()
+train_x, train_y = gen_training(500)
 train_set = gen_test(200)
+
+for i in range(4):
+    e = 10 ** -(i + 1)
+    w0 = nn.w_vec()
+    grad = nn.eval_grad_of_loss_all(train_x, train_y)
+    # func -  feed forward and calculate loss for a given W where x and y are constants
+    # gradient - calculate for a given W where x and y are constants
+    # desc = bfgs.BFGS(func, nn.eval_grad_of_loss_all(train_x, train_y), w0, bfgs.inexact_line_search, False, e)[1]
